4698d33ce6351aa2773648d009c3819c33c1fb18
Trevor Dorsey <tsdorsey@gmail.com>
2016-09-28 11:33:00 -0700

    Fetch to folder. Walk folder. Fip codes in arrays.
    Remove hardcoded file count/naming:
      1. Download the zip files into a subfolder
      2. Extract into subfolder
      3. Walk the folder for files
    
    The values in the final JSON object are arrays now. A single zip code
    can span multiple counties.

--- makejson.py
+++ makejson.py
@@ -2,17 +2,34 @@
 
 import json
 import re
+import os
 
 statecodes = json.load(open('state_fips.json'))
 zipmap = {}
+cityFolder = 'zipctys/extracted'
 
-for i in range(1,11):
-    zfile = open('zipctys/zipcty%d' % i)
-    zfile.readline() # skip first line
-    for l in zfile:
-        m = re.match(r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)", l)       
-        if m:
-            r = m.groupdict()
-            zipmap[r['zip']] = statecodes[r['state']] + r['fips']
+for _, _, paths in os.walk(cityFolder):
+    # Filter out system/hidden files.
+    paths = [path for path in paths if path[0] != '.']
+    
+    print "Found {count} data files to process.".format(count=len(paths))
+    for index, path in enumerate(paths):
+        print "Processing {name} - {x} of {y}".format(name=path, x=(index+1), y=(len(paths)))
+        with open(os.path.join(cityFolder, path), 'r') as zfile:
+            zfile.readline() # skip first line
+            for l in zfile:
+                l = l.strip() # Clean the whitespace off the front and back of the line.
+                m = re.match(r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)", l)
+                if m:
+                    r = m.groupdict()
+                    zipmap.setdefault(r['zip'], set()).add(statecodes[r['state']] + r['fips'])
+                else:
+                    print 'Failed to parse: "{line}"'.format(line=l)
 
-print json.dumps(zipmap)
+# Convert sets to lists so they can be serialized.
+print ""
+for key in zipmap.keys():
+    zipmap[key] = sorted(zipmap[key])
+
+with open('zip2fips.json', 'w') as out:
+    out.write(json.dumps(zipmap))
